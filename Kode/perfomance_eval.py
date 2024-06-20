4214b2d9c6f9478ef2517d714554319bb1b719ef
Zilverwood <zilverwood@gmail.com>
2024-06-20 13:54:00 +0200

    weewoo

---
+++ Kode/perfomance_eval.py
@@ -0,0 +1,52 @@
+from sklearn.metrics import precision_recall_curve, roc_curve, auc, average_precision_score, roc_auc_score
+from sklearn.model_selection import GroupKFold
+
+def evaluate_model(X_withid, X, y, pred_func, model_name):
+
+    outer_cv = GroupKFold(n_splits = 5)
+    inner_cv = GroupKFold(n_splits = 5)
+
+    results = {
+    'lr': {'pr_auc': [], 'roc_auc': [], 'precision': [], 'recall': [], 'fpr': [], 'tpr': []},
+    'rf': {'pr_auc': [], 'roc_auc': [], 'precision': [], 'recall': [], 'fpr': [], 'tpr': []},
+    'gbm': {'pr_auc': [], 'roc_auc': [], 'precision': [], 'recall': [], 'fpr': [], 'tpr': []},
+    'mlp': {'pr_auc': [], 'roc_auc': [], 'precision': [], 'recall': [], 'fpr': [], 'tpr': []}}
+
+    for outer_train_idx, outer_test_idx in outer_cv.split(X, y, groups = X_withid['IDno']):
+        X_outer_train, y_outer_train = X.iloc[outer_train_idx], y.iloc[outer_train_idx]
+        X_outer_test, y_outer_test = X.iloc[outer_test_idx], y.iloc[outer_test_idx]
+
+        inner_cv = GroupKFold(n_splits=5)
+        inner_pr_auc_scores = []
+        inner_roc_auc_scores = []
+
+        for inner_train_idx, inner_test_idx in inner_cv.split(X_outer_train, y_outer_train, groups=X_withid['IDno'].iloc[outer_train_idx]):
+            X_inner_train, y_inner_train = X_outer_train.iloc[inner_train_idx], y_outer_train.iloc[inner_train_idx]
+            X_inner_test, y_inner_test = X_outer_train.iloc[inner_test_idx], y_outer_train.iloc[inner_test_idx]
+
+            y_pred_prob = pred_func(X_inner_train, y_inner_train, X_inner_test)
+    
+            precision, recall, _ = precision_recall_curve(y_inner_test, y_pred_prob)
+            pr_auc = auc(recall, precision)
+            inner_pr_auc_scores.append(pr_auc)
+
+            fpr, tpr, _ = roc_curve(y_inner_test, y_pred_prob)
+            roc_auc = roc_auc_score(y_inner_test, y_pred_prob)
+            inner_roc_auc_scores.append(roc_auc)
+
+        y_pred_prob = pred_func(X_outer_train, y_outer_train, X_outer_test)
+
+        # Compute PR AUC
+        precision, recall, _ = precision_recall_curve(y_outer_test, y_pred_prob)
+        pr_auc = auc(recall, precision)
+        results[model_name]['pr_auc'].append(pr_auc)
+
+        # Compute ROC AUC
+        fpr, tpr, _ = roc_curve(y_outer_test, y_pred_prob)
+        roc_auc = roc_auc_score(y_outer_test, y_pred_prob)
+        results[model_name]['roc_auc'].append(roc_auc)
+
+        results[model_name]['precision'].append(precision)
+        results[model_name]['recall'].append(recall)
+        results[model_name]['fpr'].append(fpr)
+        results[model_name]['tpr'].append(tpr)
